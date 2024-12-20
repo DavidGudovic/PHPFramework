ae263445e6959e40f34531781c39cb696a4f92c9
David <david.gudovic@datadesign.me>
2024-12-20 15:38:57 +0100

    twig added to project and services

--- config/services.php
+++ config/services.php
@@ -1,13 +1,21 @@
 <?php
 
-use Dgudovic\Framework\Http\Kernel;
-use Dgudovic\Framework\Routing\Router;
-use Dgudovic\Framework\Routing\RouterInterface;
-use League\Container\Argument\Literal\ArrayArgument;
-use League\Container\Argument\Literal\StringArgument;
-use League\Container\Container;
-use League\Container\ReflectionContainer;
+use Dgudovic\Framework\{
+    Http\Kernel,
+    Routing\Router,
+    Routing\RouterInterface
+};
+use League\Container\{
+    Argument\Literal\ArrayArgument,
+    Argument\Literal\StringArgument,
+    Container,
+    ReflectionContainer
+};
 use Symfony\Component\Dotenv\Dotenv;
+use Twig\{
+    Environment,
+    Loader\FilesystemLoader
+};
 
 $dotenv = new Dotenv();
 $dotenv->load(BASE_PATH . '/.env');
@@ -20,11 +28,18 @@ $container->delegate(new ReflectionContainer(true));
 $routes = require_once BASE_PATH . '/routes/web.php';
 $appEnv = $_ENV['APP_ENV'];
 
+$templatesPath = BASE_PATH . '/templates';
+
 $container->add('APP_ENV', new StringArgument($appEnv));
 
 # services
 $container->add(RouterInterface::class, Router::class)->addMethodCall('setRoutes', [new ArrayArgument($routes)]);
 
-$container->add(Kernel::class)->addArgument(RouterInterface::class)->addArgument($container);
+$container->add(Kernel::class)
+    ->addArgument(RouterInterface::class)
+    ->addArgument($container);
+
+$container->addShared('filesystem-loader', FilesystemLoader::class)->addArgument(new StringArgument($templatesPath));
+$container->addShared(Environment::class)->addArgument('filesystem-loader');
 
 return $container;
