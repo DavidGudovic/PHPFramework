ff482b1d0b6d0b5cc7c7c05c16d1fd3e69c0e9bb
David <david.gudovic@datadesign.me>
2024-12-20 15:22:43 +0100

    status code consts

--- framework/src/Http/Response.php
+++ framework/src/Http/Response.php
@@ -6,6 +6,12 @@ use JetBrains\PhpStorm\NoReturn;
 
 readonly class Response
 {
+    public const HTTP_INTERNAL_SERVER_ERROR = 500;
+    public const HTTP_OK = 200;
+    public const HTTP_NOT_FOUND = 404;
+    public const HTTP_BAD_REQUEST = 400;
+    public const HTTP_UNAUTHORIZED = 401;
+    public const HTTP_FORBIDDEN = 403;
     public function __construct(
         public ?string $content = '',
         public int     $status = 200,
