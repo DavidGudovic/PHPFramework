1f22ed6a8be9567ecdd3e269612532b7552668a8
David <david.gudovic@datadesign.me>
2025-01-01 22:17:26 +0100

    added posts table migration

--- migrations/20230304203000.php
+++ migrations/20230304203000.php
@@ -1,14 +1,21 @@
 <?php
 
-return new class
-{
-    public function up(): void
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Types;
+
+return new class {
+    public function up(Schema $schema): void
     {
-        echo get_class($this) . ' "up" method called' . PHP_EOL;
+        $table = $schema->createTable('posts');
+        $table->addColumn('id', Types::INTEGER, ['autoincrement' => true, 'unsigned' => true]);
+        $table->addColumn('title', Types::STRING, ['length' => 255]);
+        $table->addColumn('body', Types::TEXT);
+        $table->addColumn('created_at', Types::DATETIME_IMMUTABLE, ['default' => 'CURRENT_TIMESTAMP']);
+        $table->setPrimaryKey(['id']);
     }
 
-    public function down(): void
+    public function down(Schema $schema): void
     {
-        echo get_class($this) . ' "down" method called' . PHP_EOL;
+        $schema->dropTable('posts');
     }
 };
